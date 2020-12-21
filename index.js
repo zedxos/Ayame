5b2c5550dade1f5750fc088bd3dbe8205ad5a286
starleyes <76438985+starleyes@users.noreply.github.com>
2020-12-21 22:20:08 +0800

    edit#5.3 Secret value testing
    The secret is a boolean + value to get access in owners level that can use eval

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ http.createServer(function (req, res) {
 }).listen(3000);
 
 /**
- * @param {string} License
+ * @param {string} MIT LICENSE
  */
 
 var AyameCore = require('./Ayame-Core/core-start.js'),
     AyameScript = require('./Ayame-Main-Modules/Ayame-Script.js'),
+    AyameSecret = 'experimenting C: /*testing*/',
     AyameToken = 'token C:';
     
 AyameCore(AyameScript, AyameToken /**@param {string} functions*/);
