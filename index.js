ff7817c6b0e524482215869ed56fb967fbbb8bf0
zedxos <69835674+zedxos@users.noreply.github.com>
2020-12-21 20:39:45 +0800

    edit#1

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ http.createServer(function (req, res) {
 
 var AyameCore = require('./Ayame-Core/core-start.js'),
     AyameScript = require('./Ayame-Main-Modules/Ayame-Script.js'),
-    AyameToken = 'NzQ1MTQwNDA4Njk0MDc5NTU5.XztcFQ.EUgqrzGv6t5q8zma-7kfBW-Vf-E';
+    AyameToken = 'token C:';
     
 AyameCore(AyameScript, AyameToken /**@param {string} functions*/);
